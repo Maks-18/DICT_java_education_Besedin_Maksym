8060412d83363114b974b16876dccb77cb9cae4c
Maks-18 <m.v.besedin@student.khai.edu>
2024-10-27 20:43:19 +0100

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,4 +1,21 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
+    public static void main(String[] args) {
+        String botName = "Star"; // Ім'я бота
+        int birthYear = 2024;     // Рік створення бота
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name:");
+
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine(); // Отримуємо ім'я користувача
+
+        System.out.println("What a great name you have, " + userName + "!");
+        scanner.close();
+    }
 }
+
