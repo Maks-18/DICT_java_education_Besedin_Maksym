31b53a73a6fb03d51de7a63f240e1a039e020bd9
Maks-18 <m.v.besedin@student.khai.edu>
2024-10-27 20:50:10 +0100

    ChatBot 4-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args) {
-        String botName = "Star"; // Ім'я бота
-        int birthYear = 2024;     // Рік створення бота
+        String botName = "Star";
+        int birthYear = 2024;
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
@@ -18,16 +18,25 @@ public class ChatBot {
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        int remainder3 = scanner.nextInt(); // Залишок від ділення на 3
-        int remainder5 = scanner.nextInt(); // Залишок від ділення на 5
-        int remainder7 = scanner.nextInt(); // Залишок від ділення на 7
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
 
-        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105; // Формула для обчислення віку
-
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+
+        System.out.println("Now I will prove to you that I can count to any number you want!");
+        System.out.println("Enter a positive number:");
+
+        int userInp = scanner.nextInt(); // Зчитуємо число, до якого потрібно рахувати
+
+        for (int i = 0; i <= userInp; i++) {
+            System.out.println(i + "!");
+        }
 
         scanner.close();
     }
 }
+
 
 
