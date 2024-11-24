fc45c83ac050be50cb2488301e1e4be56765a869
Maks-18 <m.v.besedin@student.khai.edu>
2024-11-24 21:29:02 +0100

    Етап 2: Проста гра з одним словом.

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -1,8 +1,26 @@
 package hangman;
+
+import java.util.Scanner;
+
 public class Hangman {
     public static void main (String[] args){
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon");
 
+        //Встановити слово, яке потрібно вгадати
+        String wordToGuess = "java";
+
+        //Запитати гравця слово
+        System.out.println("what kind of word did i guess?");
+        Scanner scanner = new Scanner(System.in);
+        String userGuess = scanner.nextLine();
+
+        //Перевірка, чи правильна відповідь
+        if (userGuess.equals(wordToGuess)) {
+            System.out.print("You win");
+        } else {
+            System.out.println("You died");
+
+        }
     }
 }
